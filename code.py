addcaf7a48b49d4ce4bfdbb1af92952917157029
Rajkumar <rajkumar@authenticate.com>
2025-08-18 11:33:54 +0530

    bad code

---
+++ code.py
@@ -0,0 +1,16 @@
+def add_numbers(a,b):
+    return a+b   # STYLE: missing spaces around operators
+
+def divide(a, b):
+    return a / b   # BUG: ZeroDivisionError not handled
+
+def inefficient_sum(numbers):
+    total = 0
+    for i in range(len(numbers)):   # PERFORMANCE: using range(len()) instead of iterating directly
+        total += numbers[i]
+    return total
+
+def insecure_function():
+    # SECURITY: dangerous use of eval
+    user_code = input("Enter Python code: ")
+    eval(user_code)
