cf556193bd9f361c36f12445cfa835a56b0394fe
Rajkumar <rajkumar@authenticate.com>
2025-08-20 23:19:13 +0530

    adding more issue

--- code.py
+++ code.py
@@ -14,3 +14,11 @@ def insecure_function():
     # SECURITY: dangerous use of eval
     user_code = input("Enter Python code: ")
     eval(user_code)
+
+def insecure_function_2():
+    # SECURITY: dangerous use of eval
+    user_code = input("Enter Python code: ")
+    exec(user_code)
+
+add = (x - 20)
+print(add)
