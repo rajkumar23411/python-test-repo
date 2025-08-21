4f700d59f73be55ce4b800103b359d11ebd14dea
Rajkumar <rajkumar@authenticate.com>
2025-08-21 08:47:59 +0530

    fixed code

--- code.py
+++ code.py
@@ -1,24 +1,11 @@
-def add_numbers(a,b):
-    return a+b   # STYLE: missing spaces around operators
+def add(a,b):
+    return a + b
 
-def divide(a, b):
-    return a / b   # BUG: ZeroDivisionError not handled
+def subtract(a,b):
+    return a - b
 
-def inefficient_sum(numbers):
-    total = 0
-    for i in range(len(numbers)):   # PERFORMANCE: using range(len()) instead of iterating directly
-        total += numbers[i]
-    return total
+def multiply(a,b):
+    return a * b
 
-def insecure_function():
-    # SECURITY: dangerous use of eval
-    user_code = input("Enter Python code: ")
-    eval(user_code)
-
-def insecure_function_2():
-    # SECURITY: dangerous use of eval
-    user_code = input("Enter Python code: ")
-    exec(user_code)
-
-def add():
-    return x + 100
+def divide(a,b):
+    return a / b
