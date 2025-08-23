bb7ec74931482652b8fa04351c6d8f2e376fa74f
Rajkumar <rajkumar@authenticate.com>
2025-08-24 01:54:30 +0530

    testing

--- code.py
+++ code.py
@@ -12,3 +12,14 @@ def divide(a,b):
 
 def power(a,b):
     return a**b
+
+def square(a):
+    return a**2
+
+def cube(a):
+    return a**3
+
+def square_root(a):
+    return a**0.5
+
+def cube_root(a):
